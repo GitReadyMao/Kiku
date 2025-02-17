0dda62d2b2e2c7ff82f45ee46d7b2406231796c1
ZackaryElmo <92867443+ZackaryElmo@users.noreply.github.com>
2025-02-16 23:07:02 -0500

    basic login interface

--- react-nextjs/src/components/ExampleOffcanvas.tsx
+++ react-nextjs/src/components/ExampleOffcanvas.tsx
@@ -3,6 +3,7 @@ import Button from "react-bootstrap/Button";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import Offcanvas from "react-bootstrap/Offcanvas";
+import Form from 'react-bootstrap/Form';
 
 export interface ExampleOffcanvasProps {
   className?: string | undefined;
@@ -18,23 +19,29 @@ const ExampleOffcanvas: React.FC<ExampleOffcanvasProps> = ({ className }) => {
       </Button>
       <Offcanvas placement="end" show={show} onHide={() => setShow(false)}>
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title as="h5">Offcanvas</Offcanvas.Title>
+          <Offcanvas.Title as="h5">Login</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          <div>
-            Some text as placeholder. In real life you can have the elements you
-            have chosen. Like, text, images, lists, etc.
-          </div>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-          <DropdownButton
-            variant="secondary"
-            title="Dropdown button"
-            className="mt-3"
-          >
-            <Dropdown.Item href="#">Action</Dropdown.Item>
-            <Dropdown.Item href="#">Another action</Dropdown.Item>
-            <Dropdown.Item href="#">Something else here</Dropdown.Item>
-          </DropdownButton>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Keep me logged in" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
         </Offcanvas.Body>
       </Offcanvas>
     </>
