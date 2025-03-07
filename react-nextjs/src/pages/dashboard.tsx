416336f94d8dd879f3c3a35f87a5ecebbc964446
ZackaryElmo <92867443+ZackaryElmo@users.noreply.github.com>
2025-03-07 01:35:22 -0500

    (very) minor cleanup

--- react-nextjs/src/pages/dashboard.tsx
+++ react-nextjs/src/pages/dashboard.tsx
@@ -1,9 +1,3 @@
-import Head from "next/head";
-import Container from "react-bootstrap/Container";
-import AppGuides from "@/components/AppGuides";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import Homepage from "@/components/Homepage";
 import ProfileBar from "@/components/ProfileBar";
 
 export default function Home() {
