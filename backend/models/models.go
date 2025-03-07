092910204b6aec60f19068254b8c4831c8d18bb4
Sean Regan <33043278+Sean-Regan@users.noreply.github.com>
2025-03-06 21:50:29 -0500

    Updated the model to reflect db

--- backend/models/models.go
+++ backend/models/models.go
@@ -1,11 +1,6 @@
 package models
 
-import (
-	"gorm.io/gorm"
-)
-
 type User struct {
-	gorm.Model
 	Email        string `json:"email"`
 	Username     string `json:"username"`
 	Password     string `json:"password"`
