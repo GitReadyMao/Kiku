ddd188bab4adc5be9941c6321fa234a931df72bc
CVMauro <maurochavezvega@ufl.edu>
2025-03-09 19:01:55 -0400

    changed cors setting to allow localhost for testing

--- backend/main.go
+++ backend/main.go
@@ -37,7 +37,7 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:3000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Content-Type", "X-CSRF-Token", "Authorization", "Origin"},
 		ExposeHeaders:    []string{"Content-Length"},
